a41daafed845cb2981e762ab0bb1eec0ecab8d98
cn0379 <cn0379@outlook.com>
2022-05-15 14:32:59 +0800

    新增 10-easy-concat

--- core/11-easy-include/template.ts
+++ core/11-easy-include/template.ts
@@ -1,1 +1,6 @@
-type Includes<T extends readonly any[], U> = any
+import { Equal } from "@type-challenges/utils";
+// 实现递归调用
+export type Includes<T extends readonly any[], U> =
+    T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true
+    ? true : Includes<Rest, U> : false  
