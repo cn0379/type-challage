720c1e5c622e04ed6da0d1577d6ea8fcb7447b3b
cn0379 <cn0379@outlook.com>
2022-06-03 19:24:22 +0800

    upcode

--- core/38-medium-kebabcase/template.ts
+++ core/38-medium-kebabcase/template.ts
@@ -1,7 +1,7 @@
 type KebabCase<S> =
     S extends `${infer S1}${infer S2}` ?
     S2 extends Uncapitalize<S2> ? `${Uncapitalize<S1>}${KebabCase<S2>}` : `${Uncapitalize<S1>}-${KebabCase<S2>}` // 这个分支判断的是剩余参数是不是大写是的话 xx-xx
-    : '1';
+    : S;
 
 
 type letter = KebabCase<'h'>
