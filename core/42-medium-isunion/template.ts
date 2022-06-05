76c2e25d7e2213234ba9ef31ec74744b3b85ec56
cn0379 <cn0379@outlook.com>
2022-06-05 19:42:52 +0800

    42-medium-isunion

---
+++ core/42-medium-isunion/template.ts
@@ -0,0 +1,9 @@
+type IsUnion<T, B = T> = T extends B ?
+    [B] extends [T] ? false : true
+    : never;
+
+
+
+type IsUnion2<T, B = T> = T extends B ? [B] extends [T] ? [T] : true : ''
+
+type tu1 = IsUnion2<'a' | 'b' | 'c' | 'd'>
