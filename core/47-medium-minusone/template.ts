4dfcf7dc5b913ea31a453cbd76b5731d8f455441
cn0379 <cn0379@outlook.com>
2022-06-09 04:14:16 +0800

    47-medium-minusone

--- core/47-medium-minusone/template.ts
+++ core/47-medium-minusone/template.ts
@@ -1,1 +1,21 @@
-type MinusOne<T extends number> = any
+// your answers
+type DigitToArray = {
+    "0": [],
+    "1": [unknown],
+    "2": [unknown, unknown],
+    "3": [unknown, unknown, unknown],
+    "4": [unknown, unknown, unknown, unknown],
+    "5": [unknown, unknown, unknown, unknown, unknown],
+    "6": [unknown, unknown, unknown, unknown, unknown, unknown],
+    "7": [unknown, unknown, unknown, unknown, unknown, unknown, unknown],
+    "8": [unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown],
+    "9": [unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown]
+  };
+  type CreateArrayByLength<N extends string, R extends unknown[] = []> = N extends `${infer First}${infer Rest}`
+  ? First extends keyof DigitToArray
+    ? CreateArrayByLength<Rest, [...R, ...R, ...R, ...R, ...R, ...R, ...R, ...R, ...R, ...R, ...DigitToArray[First]]>
+    : never
+  : R;
+  type MinusOne<T extends number> = CreateArrayByLength<`${T}`> extends [infer First, ...infer Rest]
+  ? Rest["length"]
+  : never;
